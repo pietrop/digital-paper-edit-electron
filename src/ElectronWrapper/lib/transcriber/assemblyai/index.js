a13d035bb915ac5caab88a1dc4eee5cac7f80343
Pietro Passarelli <pietro.passarelli@wsj.com>
2021-02-01 22:36:58 -0500

    1.3.2
    added updated transcript editor with improved alignment performance

--- src/ElectronWrapper/lib/transcriber/assemblyai/index.js
+++ src/ElectronWrapper/lib/transcriber/assemblyai/index.js
@@ -1,31 +1,32 @@
-const  assemblyai  = require('@pietrop/assemblyai');
+const fs = require('fs');
+const assemblyai = require('@pietrop/assemblyai');
 
 const { getCredentials, areCredentialsSet } = require('../../../../stt-settings/credentials.js');
 
-const sampleJson = require('./assemblyai-to-dpe/assemblyai-sample.json');
+// const sampleJson = require('./assemblyai-to-dpe/assemblyai-sample.json');
 
-async function assemblyAiStt (filePath, language, languageModel) {
+async function assemblyAiStt(filePath, language, languageModel) {
   let assemblyAiCredentials;
   if (areCredentialsSet('AssemblyAI')) {
- assemblyAiCredentials = getCredentials('AssemblyAI');
- const  ApiKey = assemblyAiCredentials.sttAPIKey;
- console.log('language' ,language ,'languageModel', languageModel);
+    assemblyAiCredentials = getCredentials('AssemblyAI');
+    const ApiKey = assemblyAiCredentials.sttAPIKey;
+    console.log('language', language, 'languageModel', languageModel);
     try {
-        const response = await assemblyai({
-          ApiKey, 
-          filePath,
-          languageModel: languageModel,
-          acousticModel: language
-       });
-
-        return response;
+      const response = await assemblyai({
+        ApiKey,
+        filePath,
+        languageModel: languageModel,
+        acousticModel: language,
+      });
+      // console.log('assemblyAi response', response);
+      // fs.writeFileSync('asssemblyAIoutput.json', JSON.stringify(response, null, 2));
+      return response;
       // }
     } catch (e) {
       // TODO: Do some error handling here
       console.error('error calling AssemblyAi SDK', e);
     }
-  }
-  else {
+  } else {
     throw new Error('No credentials found for AssemblyAI');
   }
 }
